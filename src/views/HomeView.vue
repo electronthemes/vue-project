fa424bfee37d052f032ad9c92715358e2c784470
Enamul Haque <enamul.iu91@gmail.com>
2024-01-23 14:03:22 +0600

    update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+  <main>
+    <SignUpFroms />
+  </main>
+</template>
+<script>
+  import SignUpFroms from '@/components/icons/SignUpFroms.vue';
+  export default {
+    components: {
+      SignUpFroms
+    },
+  }
+</script>
